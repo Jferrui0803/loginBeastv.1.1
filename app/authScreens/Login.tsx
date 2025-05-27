dd292815c200054d5e38abd04aeca57ebae8df5c
Carlosrucar <crodrui1905@ieszaidinvergeles.org>
2025-05-28 01:22:14 +0200

    Cambios Login

--- app/authScreens/Login.tsx
+++ app/authScreens/Login.tsx
@@ -1,112 +1,131 @@
-import { View, StyleSheet, ScrollView } from 'react-native';
-import React, { useState } from 'react';
-import { Text, TextInput, Button, Surface } from 'react-native-paper';
-import { useAuth } from '../context/AuthContext';
-import { useNavigation } from '@react-navigation/native';
+import { View, StyleSheet, Image } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { TextInput, Button, Text, Surface } from 'react-native-paper';
+import { API_URL, useAuth } from '../context/AuthContext';
+import axios from 'axios';
 
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const { onLogin } = useAuth();
-    const navigation = useNavigation();
+    const [secureTextEntry, setSecureTextEntry] = useState(true);
+    const { onLogin, } = useAuth(); 
+    
+    useEffect(() => {
+        const testCall = async () => {
+            const result = await axios.get(`${API_URL}/api/auth/login`);
+        }
+        testCall();
+    }, []);
 
-    const handleLogin = async () => {
+    const login = async () => {
         try {
             const result = await onLogin!(email, password);
-            if (result && result.error) {
+            if (result.error) {
                 alert(result.msg);
+            } else {
+                console.log('Login successful', result.data);
             }
         } catch (error) {
             if (error instanceof Error) {
-                alert('Error al iniciar sesión: ' + error.message);
+                alert('Error inesperado: ' + error.message);
             } else {
-                alert('Error al iniciar sesión');
+                alert('Error inesperado');
             }
             console.error(error);
         }
     };
 
+
+
     return (
-        <ScrollView contentContainerStyle={styles.scrollView}>
-            <View style={styles.container}>
-                <Surface style={styles.formContainer} elevation={2}>
-                    <Text style={styles.title}>Iniciar Sesión</Text>
+        <View style={styles.container}>
+            <Surface style={styles.formContainer} elevation={2}>
+                <Image 
+                    source={require('../assets/image.png')} 
+                    style={styles.logo}
+                    resizeMode="contain"
+                />
+                <Text style={styles.title}>GYMFIT</Text>
+                <TextInput
+                    label="Email"
+                    value={email}
+                    onChangeText={setEmail}
+                    mode="outlined"
+                    style={styles.input}
+                    keyboardType="email-address"
+                    autoCapitalize="none"
+                    outlineColor="black"
+                    activeOutlineColor="black"
+                />
+                <TextInput
+                    label="Contraseña"
+                    value={password}
+                    onChangeText={setPassword}
+                    mode="outlined"
+                    style={styles.input}
+                    secureTextEntry={secureTextEntry}
+                    outlineColor="black"
+                    activeOutlineColor="black"
+                    right={
+                        <TextInput.Icon
+                            icon={secureTextEntry ? "eye" : "eye-off"}
+                            onPress={() => setSecureTextEntry(!secureTextEntry)}
+                        />
+                    }
+                />
+                <Button 
+                    mode="contained" 
+                    onPress={login}
+                    style={{ backgroundColor: '#ffa500' }}
+                    textColor="white" 
+                >
+                    Iniciar Sesión
+
+                </Button>
                     
-                    <TextInput
-                        label="Email"
-                        value={email}
-                        onChangeText={setEmail}
-                        mode="outlined"
-                        style={styles.input}
-                        keyboardType="email-address"
-                        autoCapitalize="none"
-                    />
 
-                    <TextInput
-                        label="Contraseña"
-                        value={password}
-                        onChangeText={setPassword}
-                        mode="outlined"
-                        style={styles.input}
-                        secureTextEntry
-                    />
-
-                    <Button 
-                        mode="contained"
-                        onPress={handleLogin}
-                        style={styles.button}
-                        buttonColor="#ffa500"
-                    >
-                        Iniciar Sesión
-                    </Button>
-
-                    <Button
-                        mode="text"
-                        onPress={() => navigation.navigate('Register')}
-                        style={styles.linkButton}
-                        textColor="#ffa500"
-                    >
-                        ¿No tienes cuenta? Regístrate
-                    </Button>
-                </Surface>
-            </View>
-        </ScrollView>
+            </Surface>
+        </View>
     );
 };
 
 const styles = StyleSheet.create({
-    scrollView: {
-        flexGrow: 1,
-    },
     container: {
         flex: 1,
-        padding: 16,
-        backgroundColor: '#f5f5dc',
+        padding: 20,
         justifyContent: 'center',
+        backgroundColor: '#f5f5dc', 
     },
     formContainer: {
-        padding: 16,
-        borderRadius: 8,
+        padding: 20,
+        borderRadius: 10,
         backgroundColor: 'white',
+    },
+    logo: {
+        width: 150,
+        height: 150,
+        alignSelf: 'center',
+
     },
     title: {
         fontSize: 24,
         fontWeight: 'bold',
-        marginBottom: 24,
         textAlign: 'center',
-        color: '#ffa500',
+        marginBottom: 20,
+        marginTop: -20,
+        color: '#333',
     },
     input: {
         marginBottom: 16,
         backgroundColor: 'white',
     },
     button: {
-        marginTop: 8,
-        marginBottom: 16,
+        marginTop: 10,
+        paddingVertical: 6,
     },
     linkButton: {
-        marginTop: 8,
-    }
+        marginTop: 10,
+    },
 });
 
 export default Login;
